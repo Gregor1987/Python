bed96ea4f1d9960ada733411fe74e3abf1a49298
Aleksandr <Gregor1987@mail.ru>
2023-06-21 09:26:43 +0500

    task 2 corrected

--- HW_1.py
+++ HW_1.py
@@ -1,37 +1,36 @@
 # Task 1. Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток.
 # Программа должна подсказывать "больше" или "меньше" после каждой попытки.
 
-# from random import randint
-#
-# num = randint(0, 1000)
-# i = 0
-# TRIES = 10
-# guess = input(f'Try to guess a number between 0 and 1000 inclusive. You have {TRIES} tries:  ')
-#
-# while i <= TRIES - 1:
-#     if i == TRIES - 1:
-#         print(f"Sorry, you don't have any attempts left. Guessed number was {num}")
-#         break
-#     elif not guess.isdigit():
-#         guess = input('incorrect input, please, try again: ')
-#         continue
-#     else:
-#         guess = int(guess)
-#         if guess < 0 or guess > 1000:
-#             guess = input('Number should be between 0 and 1000, please, try again: ')
-#             continue
-#         elif guess > num:
-#             i += 1
-#             guess = input(f'Guessed number is smaller, try again. You have {TRIES - i} tries left: ')
-#             continue
-#         elif guess < num:
-#             i += 1
-#             guess = input(f'Guessed number is greater, try again. You have {TRIES - i} tries left: ')
-#             continue
-#         elif guess == num:
-#             print('Bravo, you guessed the right number!')
-#             break
-#
+from random import randint
+
+num = randint(0, 1000)
+i = 0
+TRIES = 10
+guess = input(f'Try to guess a number between 0 and 1000 inclusive. You have {TRIES} tries:  ')
+
+while i <= TRIES - 1:
+    if i == TRIES - 1:
+        print(f"Sorry, you don't have any attempts left. Guessed number was {num}")
+        break
+    elif not guess.isdigit():
+        guess = input('incorrect input, please, try again: ')
+        continue
+    else:
+        guess = int(guess)
+        if guess < 0 or guess > 1000:
+            guess = input('Number should be between 0 and 1000, please, try again: ')
+            continue
+        elif guess > num:
+            i += 1
+            guess = input(f'Guessed number is smaller, try again. You have {TRIES - i} tries left: ')
+            continue
+        elif guess < num:
+            i += 1
+            guess = input(f'Guessed number is greater, try again. You have {TRIES - i} tries left: ')
+            continue
+        elif guess == num:
+            print('Bravo, you guessed the right number!')
+            break
 
 # Task 2. Треугольник существует только тогда, когда сумма любых двух его сторон больше третьей.
 # Дано a, b, c - стороны предполагаемого треугольника. Требуется сравнить длину каждого отрезка-стороны с
@@ -39,11 +38,21 @@
 # такими сторонами не существует. Отдельно сообщить является ли треугольник разносторонним,
 # равнобедренным или равносторонним.
 
-a = input('Side 1: ')
-b = input('Side 2: ')
-c = input('Side 3: ')
+
+def side_length(side_number):
+    side = input(f'Please, input side {side_number} length: ')
+    while not side.isdigit():
+        side = input('Incorrect input, please try again: ')
+    side = int(side)
+    return side
+
+
+a = side_length('1')
+b = side_length('2')
+c = side_length('3')
+
 if a > b + c or b > a + c or c > a + b:
-    print('such traingle cannot not exist')
+    print('such triangle cannot not exist')
 else:
     print('This is', end=' ')
     if a == b == c:
