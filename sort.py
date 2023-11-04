09450ede1e4128a9d0e79cf880cc57451cb5ae6a
Aleksandr <Gregor1987@mail.ru>
2023-11-04 20:29:00 +0500

    added sort

---
+++ sort.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+
+def sort(folder, **kwargs):
+    print(kwargs)
+    for _ , _,  files in os.walk(folder):
+        for file in files:
+            ext = file.split('.')[-1]
+            for item in kwargs:
+                if ext in kwargs[item]:
+                    if not os.path.isdir(item):
+                        os.mkdir(item)
+                    shutil.move(f'{folder}/{file}', item)
+
+
+sort('new_folder', video=['avi', 'mov'], images=['jpg', 'png'], text=['txt', 'doc'])
